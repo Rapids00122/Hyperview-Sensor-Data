60c11e5569988c12383b1368d45e464dd80bdb09
Rapids <crinionr@msu.edu>
2024-09-18 13:53:06 -0400

    removeed v2 project names

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Richard\Desktop\Sensor Project v2\.venv
+set VIRTUAL_ENV=C:\Users\Richard\Desktop\Hyperview-Sensor-Data\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
